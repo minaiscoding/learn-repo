9e8eec21b40981c097ab6d398fefda883b6e29a5
minaiscoding <aminakhadir04kh@gmail.com>
2022-08-11 22:50:35 +0100

    added an action

---
+++ .github/scripts/challenge.py
@@ -0,0 +1,39 @@
+
+
+
+cat = 'reverse'
+dif = 'hard'
+data = ['Challenges : 0\n', 'Reverse : 0\n', 'Crypto : 0\n', 'per dif:\n', 'Easy : 0\n', 'Hard : 0\n']
+try:
+    file = open('./save.txt','r')
+except FileNotFoundError:
+    file = open('./save.txt','w')
+    for line in data:
+        file.write(line)
+    file.close()
+    file = open('./save.txt','r')
+with open('./save.txt','r') as file:
+    data = file.readlines()
+    num = str(int(data[0][-2])+1) 
+    data[0] = f'Challenges : {num}\n'
+    print(data)
+    match cat.upper():
+        case 'CRYPTO':
+            num = str(int(data[2][-2])+1) 
+            data[2] = f'Crypto : {num}\n' 
+# string doesn't support item assignment that's why i'm rewriting the whole line
+        case 'REVERSE':
+            num = str(int(data[1][-2])+1) 
+            data[1] = f'Reverse : {num}\n'
+    match dif.upper():
+        case 'EASY':
+            num = str(int(data[4][-2])+1) 
+            data[4] = f'Easy : {num}\n'
+        case 'HARD':
+            num = str(int(data[5][-2])+1) 
+            data[5] = f'Hard : {num}\n'
+with open('./save.txt','w') as file:
+    for line in data:
+        file.write(line)
+
+
